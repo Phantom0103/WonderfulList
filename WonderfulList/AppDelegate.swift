7fe3359ecb2fbd2064c3d4914af7b60b2b995ccf
Phantom <vszhou_wei2007@126.com>
2020-03-31 11:58:54 +0800

    请求通知权限（注释）

--- WonderfulList/AppDelegate.swift
+++ WonderfulList/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+//import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // 请求通知权限
+        //UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (accepted, error) in}
         
         return true
     }
